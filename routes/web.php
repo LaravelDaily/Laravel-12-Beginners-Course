2cba63578505d1c709ce1b34a159dc955783c772
PovilasKorop <povilask007@gmail.com>
2025-09-30 15:42:43 +0300

    Categories on sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/article', 'article')->name('article');
