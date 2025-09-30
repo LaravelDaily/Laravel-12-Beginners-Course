c44987984289179e08a7fb761cd4bfba2e9e79a7
PovilasKorop <povilask007@gmail.com>
2025-09-30 13:41:05 +0300

    About/contact layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-3xl font-bold">About Us</h2>
+    <p class="mt-4 text-lg text-gray-600">Learn more about us.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('/second-page', 'second');
-Route::view('/third-page', 'third');
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
